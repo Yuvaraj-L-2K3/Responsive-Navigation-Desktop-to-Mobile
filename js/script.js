7ec5f3a129223d5a1f49db298fc99131b1b7be7f
Yuvaraj-L-2K3 <yuviyuvaraj207@gmail.com>
2024-12-20 22:49:15 +0530

    v1

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const btn = document.getElementsByClassName("btn")[0]
+const navbar=document.getElementsByClassName('navbar-link')[0]
+btn.addEventListener('click',()=>{
+navbar.classList.toggle('active');
+});
